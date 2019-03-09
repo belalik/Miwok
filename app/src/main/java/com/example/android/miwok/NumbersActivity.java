990e58742ef6ae46f5a5764c1540cec89ad841a9
tkogias <tkogias@aegean.gr>
2019-03-09 02:25:27 +0200

    ok

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -13,19 +17,38 @@ public class NumbersActivity extends AppCompatActivity {
 
         // create an array of words
 
-        String[] words = new String[10];
+        //String[] words = new String[10];
 
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<String>();
 
-        Log.v("NumbersActivity", "word at index 9 is: "+words[9]);
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        Log.v("NumbersActivity", "word at ArrayList index 9 is: " + words.get(9));
+
+        // find the root view of the whole layout
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+
+        for (int i = 0; i < words.size(); i++) {
+            // create a new textView
+            TextView wordView = new TextView(this);
+
+            // set the text to be the word at the current index
+            wordView.setText(words.get(i));
+
+            // Add this textView as another child to the root view of this layout
+            rootView.addView(wordView);
+        }
+
+
     }
 }
